fa17b09ee6a5110f725655c977e95c2b60cb14e6
Julie-Onigiri <loptibus@gmail.com>
2024-02-19 14:57:57 +0100

    challenge-space-JS

--- js/appV1.js
+++ js/appV1.js
@@ -1,39 +1,11 @@
 
 
 // ton code ici
-
-function handleSubmit(event) {
-  //* il existe une methode que nous permet de stoper l'evenement par defaut
-  //* du formulaire
-  //* par defaut le form de html renvoie vers un autre lien
- event.preventDefault();
-  console.dir(event.target);
- const formulaire = event.target;
-  //* Pour recuperer les input à l'interieur du formulaire
-  //* 1ere methode
-  const inputName = formulaire[0];
-  console.log(inputName);
-  console.log(inputName.value); 
-  //* 2eme methode
-  //*let inputForm = formulaire.querySelector('configuration')
-  //*console.log(inputForm.value);
-
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', handleSubmit);
-
-}
-
-
-
-
-
 const grille = document.createElement("div");
 grille.id = "grille"; // Donner un idà la grille
 grille.style.border="2px solid black"
-grille.style.width = "200px"; // Définir la largeur de la grille
-grille.style.height = "200px"; // Définir la hauteur de la grille
+grille.style.width = "100px"; // Définir la largeur de la grille
+grille.style.height = "100px"; // Définir la hauteur de la grille
 grille.style.display = "grid"; // Utiliser le modèle de mise en page grid
 grille.style.gridTemplateColumns = "repeat(8, 0.6fr)"; // Créer 8 colonnes de taille égale
 grille.style.gridTemplateRows = "repeat(8, 0.6fr)"; // Créer 8 lignes de taille égale
@@ -60,19 +32,17 @@ for (let i = 0; i < 64; i++) {
     // Ajouter la case à la grille
     grille.appendChild(caseElement);
 
-    
-    caseElement.onclick = function(){
+   caseElement.onclick = function(){
       if(caseElement.style.backgroundColor === "grey"){
-         caseElement.style.backgroundColor = "black";
+         caseElement.style.backgroundColor = "red";
         }
-        else if(caseElement.style.backgroundColor === "black"){
-        caseElement.style.backgroundColor = "grey";
+        else{
+        caseElement.style.backgroundColor = getRandomColor();
       }
-};
- 
+  };
   }
   console.log(grille)
-  /*const caseElement= document.getElementById('valid');
+  const caseElement= document.getElementById('valid');
   caseElement.addEventListener('click',ceQuiFautFaire);
   /*let x;
   let y;
@@ -85,18 +55,7 @@ for (let i = 0; i < 64; i++) {
   }
 //* creer une function qui va a chq boucle creer une case de 10*10 width:10px heigth:10px 
 //*avec un border de 1px solid grey  arrivé à la huitieme case zouuuh on descend de 12px y+12
-*/
-const btnGrisClair= document.getElementById('item1');
-const btnGris= document.getElementById('item2');
-const btnOrange= document.getElementById('item3');
-const btnVert = document.getElementById('item4');
+ */
 
-const color = [btnGrisClair, btnGris, btnOrange, btnVert];
 
-// colorpicker {
-//  item1.addEventListener('click', function {
-//  item1.style.border.opacity = 1;
-//  color()
-//  
-// })
-//}
+
